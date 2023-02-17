2dd7c76e2e64f7f61c649f5323c91352d7a32252
Mosiah Batista <62183086+mosiahdx@users.noreply.github.com>
2023-02-17 10:14:44 -0300

    Add files via upload

---
+++ terminal.js
@@ -0,0 +1,5 @@
+var entrada = require("readline-sync");
+var n1 = parseFloat(entrada.question("Digite um numero: "));
+var n2 = parseFloat(entrada.question("Digite outro numero: "));
+var resultado = n1 - n2;
+console.log(resultado);
---
+++ terminal.ts
@@ -0,0 +1,9 @@
+const entrada = require("readline-sync");
+ 
+let n1= entrada.question("Digite um numero: ");
+let n2= entrada.question("Digite outro numero: ");
+
+let resultado = n1 + n2
+
+
+console.log( resultado ); 
